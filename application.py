00291798e218f91146ac1f357c72a58769b597ae
Kanishka Narayan <37234086+kanishkan91@users.noreply.github.com>
2019-03-22 15:22:26 -0400

    Update application.py

--- application.py
+++ application.py
@@ -3,140 +3,144 @@ import pandas as pd
 
 import json
 
+#dfP=pd.read_excel(r'C:\Users\Public\Pythonfiles\CropsFull.xlsx')
 
 
 
-
-application= Flask(__name__)
-
+app= Flask(__name__)
 
 class DataStore():
     CountryName=None
     Year=None
+    Prod= None
+    Loss=None
 data=DataStore()
 
 
-@application.route("/main",methods=["GET","POST"])
+@app.route("/main",methods=["GET","POST"])
 
-@application.route("/",methods=["GET","POST"])
+@app.route("/",methods=["GET","POST"])
 def homepage():
     CountryName = request.form.get('Country_field','India')
-    
+    print(CountryName)
     Year = request.form.get('Year_field', 2013)
     data.CountryName=CountryName
     data.Year=Year
+    df = pd.read_csv('CropsFull.csv')
+    # dfP=dfP
+    CountryName = data.CountryName
+    data.CountryName = CountryName
 
-    return render_template("index.html",CountryName=CountryName,Year=Year)
+    # print(CountryName)
+    #Year = data.Year
+    #data.Year = Year
+
+    # choose columns to keep, in the desired nested json hierarchical order
+    df = df[df.Country == CountryName]
+    df = df[df.Year == int(Year)]
+    print(df.head())
+    # df = df.drop(
+    # ['Country', 'Item Code', 'Flag', 'Unit', 'Year Code', 'Element', 'Element Code', 'Code', 'Item'], axis=1)
+    df = df[["Category", "Cat", "value"]]
+
+    # order in the groupby here matters, it determines the json nesting
+    # the groupby call makes a pandas series by grouping 'the_parent' and 'the_child', while summing the numerical column 'child_size'
+    df1 = df.groupby(['Category', 'Cat'])['value'].sum()
+    df1 = df1.reset_index()
+
+    # start a new flare.json document
+    flare = dict()
+    d = {"name": "flare", "children": []}
+
+    for line in df1.values:
+        Category = line[0]
+        Cat = line[1]
+        value = line[2]
+
+        # make a list of keys
+        keys_list = []
+        for item in d['children']:
+            keys_list.append(item['name'])
+
+        # if 'the_parent' is NOT a key in the flare.json yet, append it
+        if not Category in keys_list:
+            d['children'].append({"name": Category, "children": [{"name": Cat, "size": value}]})
+
+        # if 'the_parent' IS a key in the flare.json, add a new child to it
+        else:
+            d['children'][keys_list.index(Category)]['children'].append({"name": Cat, "size": value})
+
+    flare = d
+    e = json.dumps(flare)
+    data.Prod = json.loads(e)
+    Prod=data.Prod
+
+    df = pd.read_csv('Losses.csv')
+    #CountryName = data.CountryName
+    #print(CountryName)
+    #Year = data.Year
+
+    # choose columns to keep, in the desired nested json hierarchical order
+    df = df[df.Country == CountryName]
+    df = df[df.Year == int(Year)]
+    print(df.head())
+    # df = df.drop(
+    # ['Country', 'Item Code', 'Flag', 'Unit', 'Year Code', 'Element', 'Element Code', 'Code', 'Item'], axis=1)
+    df = df[["Category", "Cat", "value"]]
+
+    # order in the groupby here matters, it determines the json nesting
+    # the groupby call makes a pandas series by grouping 'the_parent' and 'the_child', while summing the numerical column 'child_size'
+    df1 = df.groupby(['Category', 'Cat'])['value'].sum()
+    df1 = df1.reset_index()
+
+    # start a new flare.json document
+    flare = dict()
+    d = {"name": "flare", "children": []}
+
+    for line in df1.values:
+        Category = line[0]
+        Cat = line[1]
+        value = line[2]
+
+        # make a list of keys
+        keys_list = []
+        for item in d['children']:
+            keys_list.append(item['name'])
+
+        # if 'the_parent' is NOT a key in the flare.json yet, append it
+        if not Category in keys_list:
+            d['children'].append({"name": Category, "children": [{"name": Cat, "size": value}]})
+
+        # if 'the_parent' IS a key in the flare.json, add a new child to it
+        else:
+            d['children'][keys_list.index(Category)]['children'].append({"name": Cat, "size": value})
+
+    flare = d
+    e = json.dumps(flare)
+    data.Loss = json.loads(e)
+    Loss = data.Loss
 
 
-@application.route("/get-data",methods=["GET","POST"])
+
+    return render_template("index.html",CountryName=CountryName,Year=Year,Prod=Prod,Loss=Loss)
+
+
+@app.route("/get-data",methods=["GET","POST"])
 def returnProdData():
-   df=pd.read_csv('CropsFull.csv')
-   CountryName = data.CountryName
-   data.CountryName=CountryName
-   Year = data.Year
-   data.Year=Year
+    f=data.Prod
 
-# choose columns to keep, in the desired nested json hierarchical order
-   df=df[df.Country==CountryName]
-   df=df[df.Year== int(Year)]
-   
-   #df = df.drop(
-       #['Country', 'Item Code', 'Flag', 'Unit', 'Year Code', 'Element', 'Element Code', 'Code', 'Item'], axis=1)
-   df = df[["Category", "Cat", "value"]]
-
-
-# order in the groupby here matters, it determines the json nesting
-# the groupby call makes a pandas series by grouping 'the_parent' and 'the_child', while summing the numerical column 'child_size'
-   df1 = df.groupby(['Category', 'Cat'])['value'].sum()
-   df1 = df1.reset_index()
-
-
-# start a new flare.json document
-   flare = dict()
-   d = {"name":"flare", "children": []}
-
-
-   for line in df1.values:
-      Category = line[0]
-      Cat = line[1]
-      value = line[2]
-
-    # make a list of keys
-      keys_list = []
-      for item in d['children']:
-        keys_list.append(item['name'])
-
-    # if 'the_parent' is NOT a key in the flare.json yet, append it
-      if not Category in keys_list:
-        d['children'].append({"name":Category, "children":[{"name":Cat, "size":value}]})
-
-    # if 'the_parent' IS a key in the flare.json, add a new child to it
-      else:
-        d['children'][keys_list.index(Category)]['children'].append({"name":Cat, "size":value})
-
-   flare = d
-   e= json.dumps(flare)
-   f= json.loads(e)
-   
-   return jsonify(f)
+    return jsonify(f)
 # export the final result to a json file
 
-@application.route("/get-loss-data",methods=["GET","POST"])
+@app.route("/get-loss-data",methods=["GET","POST"])
 def returnLossData():
-   df=pd.read_csv('Losses.csv')
-   CountryName = data.CountryName
-   data.CountryName=CountryName
-   Year = data.Year
-   data.Year=Year
+    g=data.Loss
 
-# choose columns to keep, in the desired nested json hierarchical order
-   df=df[df.Country==CountryName]
-   df=df[df.Year== int(Year)]
-   
-   #df = df.drop(
-       #['Country', 'Item Code', 'Flag', 'Unit', 'Year Code', 'Element', 'Element Code', 'Code', 'Item'], axis=1)
-   df = df[["Category", "Cat", "value"]]
-
-
-# order in the groupby here matters, it determines the json nesting
-# the groupby call makes a pandas series by grouping 'the_parent' and 'the_child', while summing the numerical column 'child_size'
-   df1 = df.groupby(['Category', 'Cat'])['value'].sum()
-   df1 = df1.reset_index()
-
-
-# start a new flare.json document
-   flare = dict()
-   d = {"name":"flare", "children": []}
-
-
-   for line in df1.values:
-      Category = line[0]
-      Cat = line[1]
-      value = line[2]
-
-    # make a list of keys
-      keys_list = []
-      for item in d['children']:
-        keys_list.append(item['name'])
-
-    # if 'the_parent' is NOT a key in the flare.json yet, append it
-      if not Category in keys_list:
-        d['children'].append({"name":Category, "children":[{"name":Cat, "size":value}]})
-
-    # if 'the_parent' IS a key in the flare.json, add a new child to it
-      else:
-        d['children'][keys_list.index(Category)]['children'].append({"name":Cat, "size":value})
-
-   flare = d
-   e= json.dumps(flare)
-   g= json.loads(e)
-   
-   return jsonify(g)
+    return jsonify(g)
 
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    app.run(debug=True)
 
 
 
